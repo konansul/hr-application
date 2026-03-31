cabab920df8cc7e81feac13c4ff749bbcda333ba
Kanan Sultanov <konansulx@gmail.com>
2026-03-31 12:58:50 +0400

    Adding new feature: HR can see applications

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://hr-application-grhdhvazdfftbna4.canadacentral-01.azurewebsites.net';
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://hr-application-grhdhvazdfftbna4.canadacentral-01.azurewebsites.net';
+//const BASE_URL = 'http://127.0.0.1:8000';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
