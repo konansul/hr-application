955002145a4589fea10790ac4782f8fab0202fe6
Kanan Sultanov <konansulx@gmail.com>
2026-05-13 14:28:12 +0400

    Update frontend origin https://hraipp.com

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,8 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "https://hraipp.com"
 ]
 
 app.add_middleware(
